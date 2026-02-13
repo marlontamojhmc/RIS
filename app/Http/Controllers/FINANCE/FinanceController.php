4cd3822ab5097a22a807763def49bcb31a908358
Marlon Tamo <marlontamo@gmail.com>
2026-02-13 12:54:36 +0800

    fixing finance approvers

--- app/Http/Controllers/FINANCE/FinanceController.php
+++ app/Http/Controllers/FINANCE/FinanceController.php
@@ -19,14 +19,19 @@ class FinanceController extends Controller
     public function index(){
         $user = auth()->user();
     
-                $applications = ApproverGroupApprover::with('application','application.accreditations','application.user')
+                $applications = ApproverGroupApprover::with('application','application.accreditations','application.user','application.approval')
                                 ->where('approver_id', auth()->id())
                                 ->orderBy('id', 'desc')
-                                ->get();
-               
+                                ->first();
+             dd($applications);
+                $approvers = ApproverGroupApprover::where('approver_group_id',$applications[0]->approver_group_id)
+                ->where('application_form_id',$applications[0]->application_form_id)             
+                ->get();
+
+                  dd($approvers);
         return Inertia::render('FSD/FINANCE/Index',[
                                 'applications'=> $applications,
-                                 
+                                 ''
                                 ]);
     }
     
